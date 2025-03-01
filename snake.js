8b9f315a9a3f0b418335b21659a607caae395256
next1code <next1code@yahoo.com>
2025-03-01 14:11:59 +0430

    make changeDirection function for the snake and match it with the arrow key to move

--- snake.js
+++ snake.js
@@ -10,6 +10,9 @@ var context;
 var snakeX = blockSize * 5;
 var snakeY = blockSize * 5;
 
+var velocityX = 0;
+var velocityY = 0;
+
 // food
 var foodX;
 var foodY;
@@ -21,18 +24,43 @@ window.onload = function() {
   context = board.getContext('2d'); // used for drawing on the board
 
   placeFood();
-  update();
+  document.addEventListener('keyup', changeDirection);
+  // update();
+  setInterval(update, 1000/10);
 }
 
 function update() {
   context.fillStyle = 'black';
   context.fillRect(0, 0, board.width, board.height);
 
-  context.fillStyle = 'lime';
-  context.fillRect(snakeX, snakeY, blockSize, blockSize);
-
   context.fillStyle = 'red';
   context.fillRect(foodX, foodY, blockSize, blockSize);
+
+  if (snakeX === foodX && snakeY === foodY) {
+    placeFood();
+  }
+
+  context.fillStyle = 'lime';
+  snakeX += velocityX * blockSize;
+  snakeY += velocityY * blockSize;
+  context.fillRect(snakeX, snakeY, blockSize, blockSize);
+  
+}
+
+function changeDirection(e) {
+  if (e.code === 'ArrowUp') {
+    velocityX = 0;
+    velocityY = -1;
+  } else if (e.code === 'ArrowDown') {
+    velocityX = 0;
+    velocityY = 1;
+  } else if (e.code === 'ArrowLeft') {
+    velocityX = -1;
+    velocityY = 0;
+  } else if (e.code === 'ArrowRight') {
+    velocityX = 1;
+    velocityY = 0;
+  }
 }
 
 function placeFood() {
