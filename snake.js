b68577c051417437c6bfd064ea20911333c5a647
next1code <next1code@yahoo.com>
2025-03-01 13:36:02 +0430

    make html, css and js file and add load code in the js file

---
+++ snake.js
@@ -0,0 +1,16 @@
+
+// board
+var blockSize = 25;
+var rows = 20;
+var cols = 20;
+var board;
+var context;
+
+window.onload = function() {
+  board = document.getElementById('board');
+  board.height = rows * blockSize;
+  board.width = cols * blockSize;
+  context = board.getContext('2d'); // used for drawing on the board
+
+  update();
+}
