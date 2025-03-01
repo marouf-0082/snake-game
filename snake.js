ffffb947fd64980cdabc99600d2256a8927c923d
next1code <next1code@yahoo.com>
2025-03-01 13:49:16 +0430

    add position of snake and its food

--- snake.js
+++ snake.js
@@ -6,6 +6,14 @@ var cols = 20;
 var board;
 var context;
 
+// snake head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+// food
+var foodX = blockSize * 10;
+var foodY = blockSize * 10;
+
 window.onload = function() {
   board = document.getElementById('board');
   board.height = rows * blockSize;
@@ -18,4 +26,10 @@ window.onload = function() {
 function update() {
   context.fillStyle = 'black';
   context.fillRect(0, 0, board.width, board.height);
+
+  context.fillStyle = 'lime';
+  context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+  context.fillStyle = 'red';
+  context.fillRect(foodX, foodY, blockSize, blockSize);
 }
