64f330af3203ed5cc12f6907282b7fdbcdad4c49
next1code <next1code@yahoo.com>
2025-03-01 13:55:18 +0430

    add a placeFood function for make a random place for the food

--- snake.js
+++ snake.js
@@ -11,8 +11,8 @@ var snakeX = blockSize * 5;
 var snakeY = blockSize * 5;
 
 // food
-var foodX = blockSize * 10;
-var foodY = blockSize * 10;
+var foodX;
+var foodY;
 
 window.onload = function() {
   board = document.getElementById('board');
@@ -20,6 +20,7 @@ window.onload = function() {
   board.width = cols * blockSize;
   context = board.getContext('2d'); // used for drawing on the board
 
+  placeFood();
   update();
 }
 
@@ -32,4 +33,11 @@ function update() {
 
   context.fillStyle = 'red';
   context.fillRect(foodX, foodY, blockSize, blockSize);
+}
+
+function placeFood() {
+  // 0-1) * cols -> (0-19.9999) -> (0-19) * 25
+  foodX = Math.floor(Math.random() * cols) * blockSize;
+  foodY = Math.floor(Math.random() * rows) * blockSize;
+  
 }
